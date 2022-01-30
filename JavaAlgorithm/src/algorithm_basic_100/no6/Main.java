f314d3f2ad8bc3144527f82f4708c3be6a8a1e0b
hyeongjun <chogudwns@naver.com>
2022-01-30 13:55:13 +0900

    Study java algorithm 22.01.30 \n\n gcd 구하기

---
+++ JavaAlgorithm/src/algorithm_basic_100/no6/Main.java
@@ -0,0 +1,21 @@
+package algorithm_basic_100.no6;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1, num2;
+        num1 = 16;
+        num2 = 8;
+
+        int smallNum = num1 < num2? num1 : num2;
+        int gcd = 1;
+
+        for(; smallNum>0 ; smallNum--){
+            if(num1 % smallNum == 0 && num2 % smallNum == 0){
+                gcd = smallNum;
+                break;
+            }
+        }
+
+        System.out.println("gcd = " + gcd);
+    }
+}
