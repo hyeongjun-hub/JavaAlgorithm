1e7c8b58f76e7c7c4d1517e39c355ce8b0ebce28
hyeongjun <chogudwns@naver.com>
2022-02-14 23:20:24 +0900

    Study java algorithm 22.02.14 BOJ 10818, BOJ 8393

---
+++ JavaAlgorithm/src/baekjoon/_8393/Main.java
@@ -0,0 +1,59 @@
+package baekjoon._8393;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int inputNum;
+
+    public static void input() {
+        FastReader fr = new FastReader();
+        inputNum = fr.nextInt();
+    }
+
+    public static void main(String[] args) {
+        input();
+        System.out.println((1+inputNum)*inputNum/2);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next(){
+            while(st == null || !st.hasMoreTokens()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        Double nextDouble(){
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
