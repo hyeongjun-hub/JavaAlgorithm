86eb33c3381d990d79054709e439c147320209ac
hyeongjun <chogudwns@naver.com>
2022-02-28 00:20:58 +0900

    Add: Implement sorting algorithm code, 22.02.28

--- JavaAlgorithm/src/sort/mergeSort.java
+++ JavaAlgorithm/src/sort/mergeSort.java
@@ -4,31 +4,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class mergeSort {
-    static ArrayList<Integer> dataList = new ArrayList<>(Arrays.asList(1, 23, 4, 56, 67, 21));
-
     public static ArrayList<Integer> split(ArrayList<Integer> dataList) {
-        if (mergeSort.dataList.size() <= 1) {
+        if (dataList.size() <= 1) {
             return dataList;
         }
-        int medium = mergeSort.dataList.size() / 2;
+        int medium = dataList.size() / 2;
 
         ArrayList<Integer> leftArr;
         ArrayList<Integer> rightArr;
 
-        leftArr = split(new ArrayList<>(mergeSort.dataList.subList(0, medium)));
-        rightArr = split(new ArrayList<>(mergeSort.dataList.subList(medium, mergeSort.dataList.size())));
+        leftArr = split(new ArrayList<>(dataList.subList(0, medium)));
+        rightArr = split(new ArrayList<>(dataList.subList(medium, dataList.size())));
 
         return merge(leftArr, rightArr);
     }
 
     public static ArrayList<Integer> merge(ArrayList<Integer> leftArr, ArrayList<Integer> rightArr) {
         ArrayList<Integer> mergedList = new ArrayList<>();
-        int leftPoint = 0, rightPoint = 0;
+        Integer leftPoint = 0;
+        Integer rightPoint = 0;
 
         // CASE 1 : 둘다 값 존재(하나라도 다 소모되면 while문 빠져나감)
         while (leftArr.size() > leftPoint && rightArr.size() > rightPoint) {
             if (leftArr.get(leftPoint) > rightArr.get(rightPoint)) {
-
+                mergedList.add(rightArr.get(rightPoint));
+                rightPoint++;
+            } else {
+                mergedList.add(leftArr.get(leftPoint));
+                leftPoint++;
             }
         }
         // CASE 2: 왼쪽 값만 존재
@@ -45,8 +48,12 @@ public class mergeSort {
         return mergedList;
     }
 
-    public static void main(String[] args) {
-        ArrayList<Integer> result = split(dataList);
-        System.out.println("result = " + result);
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<Integer> testData = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            testData.add((int) (Math.random() * 100));
+        }
+        split(testData);
+        System.out.println(testData);
     }
 }
