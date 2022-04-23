30b93bf4a77af30b424e556b5c1143c9b49a7888
hyeongjun <chogudwns@naver.com>
2022-04-23 19:15:18 +0900

    add: Solve java algorithm problem, 22.04.23 백준 1001, 3052, 10172

---
+++ JavaAlgorithm/src/baekjoon/_1001/Main.java
@@ -0,0 +1,69 @@
+package baekjoon._1001;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A-B를 출력한다.
+ */
+public class Main {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next(){
+            while(st == null || !st.hasMoreTokens()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        Double nextDouble(){
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    static int A;
+    static int B;
+
+    public static void input() {
+        FastReader fr = new FastReader();
+        A = fr.nextInt();
+        B = fr.nextInt();
+    }
+    public static void main(String[] args) {
+        input();
+        System.out.println(A - B);
+    }
+}
