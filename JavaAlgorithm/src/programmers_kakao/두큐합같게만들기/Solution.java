f412617cc5d12587f2a0d63551273049ccd1032f
hyeongjun <chogudwns@naver.com>
2022-11-27 14:57:52 +0900

    add: Solve java algorithm problem, 22.11.27

---
+++ JavaAlgorithm/src/programmers_kakao/두큐합같게만들기/Solution.java
@@ -0,0 +1,41 @@
+package programmers_kakao.두큐합같게만들기;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+
+    public int solution(int[] queue1, int[] queue2) {
+        Queue<Long> longQueue1 = new LinkedList<>();
+        Queue<Long> longQueue2 = new LinkedList<>();
+        long sum1 = 0, sum2 = 0, sum;
+        for (long num1 : queue1) {
+            sum1 += num1;
+            longQueue1.add(num1);
+        }
+        for (long num2 : queue2) {
+            sum2 += num2;
+            longQueue2.add(num2);
+        }
+        sum = sum1 + sum2;
+        if(sum%2==1) return -1;
+        sum/=2;
+        int limit = queue1.length*2;
+        int r1 = 0, r2 =0;
+        while (r1 <= limit && r2 <= limit) {
+            if (sum1 == sum) return r1 + r2;
+            if (sum1 > sum) {
+                sum1 -= longQueue1.peek();
+                sum2 += longQueue1.peek();
+                longQueue2.add(longQueue1.poll());
+                r1++;
+            } else {
+                sum2 -= longQueue2.peek();
+                sum1 += longQueue2.peek();
+                longQueue1.add(longQueue2.poll());
+                r2++;
+            }
+        }
+        return -1;
+    }
+}
---
+++ JavaAlgorithm/src/programmers_kakao/성격유형검사하기/Solution.java
@@ -0,0 +1,51 @@
+package programmers_kakao.성격유형검사하기;
+
+import java.util.HashMap;
+
+class Solution {
+    static HashMap<String, Integer> map = new HashMap<>();
+
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+
+        for (int i = 0; i < survey.length; i++) {
+            givePoint(survey[i], choices[i]);
+        }
+
+        answer = getAnswer();
+        return answer;
+    }
+
+    private String getAnswer() {
+        StringBuilder sb = new StringBuilder();
+
+        if (map.getOrDefault("R", 0) >= map.getOrDefault("T", 0)) {
+            sb.append("R");
+        } else sb.append("T");
+        if (map.getOrDefault("C", 0) >= map.getOrDefault("F", 0)) {
+            sb.append("C");
+        } else sb.append("F");
+        if (map.getOrDefault("J", 0) >= map.getOrDefault("M", 0)) {
+            sb.append("J");
+        } else sb.append("M");
+        if (map.getOrDefault("A", 0) >= map.getOrDefault("N", 0)) {
+            sb.append("A");
+        } else sb.append("N");
+
+        return sb.toString();
+    }
+
+    private void givePoint(String s, int choice) {
+        if(choice == 4) return;
+        if (choice < 4) {
+            map.put(s.substring(0, 1), map.getOrDefault(s.substring(0,1), 0) + 4 - choice);
+        } else {
+            map.put(s.substring(1, 2), map.getOrDefault(s.substring(1,2), 0) + choice - 4);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(new String[]{"AN", "CF", "MJ", "RT", "NA"}, new int[]{5, 3, 2, 7, 5}));
+    }
+}
