05622e875002bd98f28b58b639db855547e15cf6
hyeongjun <chogudwns@naver.com>
2023-02-04 13:44:14 +0900

    add: Solve java algorithm problem, 23.02.04

--- JavaAlgorithm/src/baekjoon/_16507/Main.java
+++ JavaAlgorithm/src/baekjoon/_16507/Main.java
@@ -8,37 +8,51 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int R, C, Q, r1, c1, r2, c2;
-    static int[][] map, row, col, hap;
+    static long[][] hap;
 
     public static void input() {
         FastReader fr = new FastReader();
         R = fr.nextInt();
         C = fr.nextInt();
         Q = fr.nextInt();
-        map = new int[R + 1][C + 1];
+        hap = new long[R + 1][C + 1];
         for (int i = 1; i <= R; i++) {
             for (int j = 1; j <= C; j++) {
-                map[i][j] = fr.nextInt();
+                hap[i][j] = fr.nextInt();
             }
         }
+        // 가로 합
         for (int i = 1; i <= R; i++) {
-            for (int j = 1; j <= C; j++) {
-
+            for (int j = 1; j < C; j++) {
+                hap[i][j+1] += hap[i][j];
             }
         }
+        // 세로 합
+        for (int i = 1; i < R; i++) {
+            for (int j = 1; j <= C; j++) {
+                hap[i + 1][j] += hap[i][j];
+            }
+        }
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < Q; i++) {
             r1 = fr.nextInt();
             c1 = fr.nextInt();
             r2 = fr.nextInt();
             c2 = fr.nextInt();
-            System.out.println(pro(r1, c1, r2, c2));
+            int ans = (int) (pro(r1, c1, r2, c2) / ((r2 - r1 + 1) * (c2 - c1 + 1)));
+            sb.append(ans);
+            sb.append('\n');
         }
+        System.out.println(sb);
+
     }
 
-    public static int pro(int r1, int c1, int r2, int c2) {
+    public static long pro(int r1, int c1, int r2, int c2) {
+        return hap[r2][c2] + hap[r1 - 1][c1 - 1] - hap[r1 - 1][c2] - hap[r2][c1 - 1];
+    }
 
-
-        return 0;
+    public static void main(String[] args) {
+        input();
     }
 
 
