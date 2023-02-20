c50300e44b7a9b93e524f30317a788f98ab7d439
hyeongjun <chogudwns@naver.com>
2023-02-20 15:14:58 +0900

    add: Solve java algorithm problem, 23.02.20

--- JavaAlgorithm/src/baekjoon/_21608/Main.java
+++ JavaAlgorithm/src/baekjoon/_21608/Main.java
@@ -8,36 +8,35 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, ans;
-    static HashMap<Integer, int[]> like = new HashMap();
-    static int[][] map;
+    static HashMap<Integer, int[]> score = new HashMap(); // 좋은하는 학생의 배열을 담는 Map
+    static int[][] map; // 교실
     static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public static void input() {
         FastReader fr = new FastReader();
         N = fr.nextInt();
         map = new int[N][N];
-        for (int i = 0; i < N*N; i++) {
+        for (int i = 0; i < N * N; i++) {
             int[] cur = new int[4];
             int index = fr.nextInt();
             for (int j = 0; j < 4; j++) {
                 cur[j] = fr.nextInt();
             }
-            like.put(index, cur);
-            place(index, cur);
+            score.put(index, cur);
+            place(index, cur); // 자리배치 시작
         }
     }
 
     private static void place(int index, int[] cur) {
-        // 1. 비어있는 칸 중 좋아하는 학생 번호가 몇 개 인접한지 +100
-        // 2. 비어있는 칸 중 인접한 빈 칸이 몇개인지 +1
-        int[][] temp = new int[N][N];
+        // 1. 비어있는 칸 중 좋아하는 학생 번호가 몇 개 인접한지 (+10)
+        // 2. 비어있는 칸 중 인접한 빈 칸이 몇개인지 (+1)
+        int[][] temp = new int[N][N]; // 임시 교실
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                temp[i][j] = -1;
-                if(map[i][j] > 0) continue;
-                // 비어있는 칸중 인접한 좋아하는 학생번호와 빈 칸 개수확인
-                int blank = 0; // 비어있는 칸
-                int like = 0;
+                temp[i][j] = -1; // -1로 초기화
+                if (map[i][j] > 0) continue;
+                // 비어있는 칸중 인접한 좋아하는 학생번호와 빈 칸 개수 확인
+                int score = 0; // 비어있는 칸의 점수
                 for (int k = 0; k < 4; k++) {
                     // 인접한 좌표
                     int newR = i + dir[k][0];
@@ -45,20 +44,20 @@ public class Main {
                     if (newR < 0 || newC < 0 || newR >= N || newC >= N) continue;
                     for (int l = 0; l < 4; l++) {
                         if (map[newR][newC] == cur[l]) {
-                            like += 100;
+                            score += 10; // 좋아하는 학생이면 10점 추가
                         }
                     }
                     if (map[newR][newC] == 0) {
-                        blank++;
+                        score++; // 빈 자리면 1점 추가
                     }
                 }
-                if(blank > temp[i][j]) temp[i][j] = blank;
-                if(like > temp[i][j]) temp[i][j] = like + blank;
+                if (score > temp[i][j]) temp[i][j] = score; // 최대값으로 갱신
             }
         }
         // 3. 순회하면서 제일 적당한 곳 찾기
         int max = -1;
-        int bestR = 0; int bestC = 0;
+        int bestR = 0;
+        int bestC = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (temp[i][j] > max) {
@@ -68,6 +67,7 @@ public class Main {
                 }
             }
         }
+        // 적당한 곳이 아무곳도 없을 때
         if (max == -1) {
             point:
             for (int i = 0; i < N; i++) {
@@ -86,15 +86,6 @@ public class Main {
     public static void main(String[] args) {
         input();
 
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < N; j++) {
-//                sb.append(map[i][j]);
-//            }
-//            sb.append("\n");
-//        }
-//        System.out.println(sb);
-
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 int people = 0;
@@ -104,15 +95,15 @@ public class Main {
                     int newC = j + dir[k][1];
                     if (newR < 0 || newC < 0 || newR >= N || newC >= N) continue;
                     for (int l = 0; l < 4; l++) {
-                        if (map[newR][newC] == like.get(map[i][j])[l]) {
+                        if (map[newR][newC] == score.get(map[i][j])[l]) {
                             people++;
                         }
                     }
                 }
-                if(people == 1) ans += 1;
-                else if(people == 2) ans+=10;
-                else if(people == 3) ans+=100;
-                else if(people == 4) ans+=1000;
+                if (people == 1) ans += 1;
+                else if (people == 2) ans += 10;
+                else if (people == 3) ans += 100;
+                else if (people == 4) ans += 1000;
             }
         }
         System.out.println(ans);
@@ -122,30 +113,38 @@ public class Main {
         BufferedReader br;
         StringTokenizer st;
 
-        public FastReader(){ br = new BufferedReader(new InputStreamReader(System.in));}
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-        String next(){
-            while(st == null || !st.hasMoreTokens()){
-                try{
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
                     st = new StringTokenizer(br.readLine());
-                } catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
 
-        int nextInt() { return Integer.parseInt(next()); }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
 
-        long nextLong() { return Long.parseLong(next()); }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
 
-        Double nextDouble() { return Double.parseDouble(next()); }
+        Double nextDouble() {
+            return Double.parseDouble(next());
+        }
 
-        String nextLine(){
+        String nextLine() {
             String str = "";
-            try{
+            try {
                 str = br.readLine();
-            } catch(IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
             return str;
