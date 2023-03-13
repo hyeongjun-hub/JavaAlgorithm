d22040f825804869bc1bc03f83cb240b4b41344e
hyeongjun <chogudwns@naver.com>
2023-03-13 19:54:40 +0900

    add:Solve java algorithm problem, 23.03.13

---
+++ JavaAlgorithm/src/baekjoon/_22251/Main.java
@@ -0,0 +1,110 @@
+package baekjoon._22251;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] digit = {
+            {1, 1, 1, 0, 1, 1, 1}, // 0
+            {0, 0, 1, 0, 0, 0, 1}, // 1
+            {0, 1, 1, 1, 1, 1, 0}, // 2
+            {0, 1, 1, 1, 0, 1, 1}, // 3
+            {1, 0, 1, 1, 0, 0, 1}, // 4
+            {1, 1, 0, 1, 0, 1, 1}, // 5
+            {1, 1, 0, 1, 1, 1, 1}, // 6
+            {0, 1, 1, 0, 0, 0, 1}, // 7
+            {1, 1, 1, 1, 1, 1, 1}, // 8
+            {1, 1, 1, 1, 0, 1, 1}, // 9
+    };
+    static int N, K, P, X, ans;
+
+    public static void input() {
+        FastReader fr = new FastReader();
+        N = fr.nextInt();
+        K = fr.nextInt();
+        P = fr.nextInt();
+        X = fr.nextInt();
+    }
+
+    public static void main(String[] args) {
+        input();
+
+        int[][] currentFloor = numToDigit(X);
+        // 현재 층을 디지털로 변환
+
+
+        for (int i = 1; i <= N; i++) {
+            // 1부터 N까지 틀린 개수 P를 안넘는 것 몇 개?
+            // rising
+            if(i == X) continue;
+            int[][] risingNum = numToDigit(i);
+
+            int diffNum = 0;
+            for (int j = 0; j < K; j++) {
+                for (int k = 0; k < 7; k++) {
+                    if(currentFloor[j][k] != risingNum[j][k]) diffNum++;
+                }
+            }
+            if(diffNum <= P) ans++;
+        }
+        System.out.println(ans);
+    }
+
+    public static int[][] numToDigit(int num) {
+        int[][] makeFloor = new int[K][7];
+
+        for (int i = 0; i < K; i++) {
+            int cur = num % 10;
+            num /= 10;
+            makeFloor[K-i-1] = digit[cur];
+        }
+
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < K; i++) {
+//            for (int j = 0; j < 7; j++) {
+//                sb.append(makeFloor[i][j]);
+//                sb.append(" ");
+//            }
+//            sb.append("\n");
+//        }
+//
+//        System.out.println(sb);
+        return makeFloor;
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader(){ br = new BufferedReader(new InputStreamReader(System.in));}
+
+        String next(){
+            while(st == null || !st.hasMoreTokens()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        Double nextDouble() { return Double.parseDouble(next()); }
+
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
